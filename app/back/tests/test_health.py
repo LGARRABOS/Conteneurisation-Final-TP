8360fb5fc4694aaf05426cc9d649655a736a0782
Luc Garrabos <92851982+LGARRABOS@users.noreply.github.com>
2025-11-20 16:11:47 +0100

    update

--- app/back/tests/test_health.py
+++ app/back/tests/test_health.py
@@ -1,5 +1,14 @@
+import os
+import sys
+
+# Ajoute le dossier parent (app/back) au PYTHONPATH, peu importe d'où pytest est lancé
+CURRENT_DIR = os.path.dirname(__file__)
+BACK_DIR = os.path.abspath(os.path.join(CURRENT_DIR, ".."))
+if BACK_DIR not in sys.path:
+    sys.path.append(BACK_DIR)
+
 from fastapi.testclient import TestClient
-from app.back.main import app  # <– changement important
+from main import app   # <= main.py dans app/back
 
 client = TestClient(app)
 
